aa740adf0cfca001c075143946aec9b2b8ff1d84
Derek <d.schlieps@gmail.com>
2018-11-16 21:42:46 +0000

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    
+    //navbar toggler
+    $('.navbar-toggler').click(function(){
+      $('.navbar-toggler').toggleClass('change')
+    });
+    
+    
+     //window scroll
+
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+    if (position >= 550) {
+      $('.navbar').addClass('navbar-background')
+      $('.navbar').addClass('fixed-top');
+      $('.navbar').removeClass('navbar-initial');
+
+    } else {
+      $('.navbar').removeClass('navbar-background');
+      $('.navbar').removeClass('fixed-top');
+      $('.navbar').addClass('navbar-initial');
+    }
+  })
+    
+});
